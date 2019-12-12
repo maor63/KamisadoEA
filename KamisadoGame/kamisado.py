7ed7a38a63663cf645dc758197e66503c3000a78
Maor <maor309@gmail.com>
2019-12-12 23:35:31 +0200

    Base

--- KamisadoGame/kamisado.py
+++ KamisadoGame/kamisado.py
@@ -27,12 +27,14 @@ class Kamisado:
     white_player_pos = {"Brown": (7, 0), "Green": (7, 1), "Red": (7, 2), "Yellow": (7, 3),
                         "Pink": (7, 4), "Purple": (7, 5), "Blue": (7, 6), "Orange": (7, 7)}
 
-    def __init__(self, black_player_pos=None, white_player_pos=None):
+    def __init__(self, black_player_pos=None, white_player_pos=None, current_player=None, tower_can_play=None):
         if black_player_pos and white_player_pos:
             self.black_player_pos = black_player_pos
             self.white_player_pos = white_player_pos
-
-
+        if current_player:
+            self.current_player = current_player
+        if tower_can_play:
+            self.tower_can_play = tower_can_play
 
     def is_legal_move(self, pos):
         y, x = pos
@@ -56,10 +58,31 @@ class Kamisado:
                 possible_moves = list(filter(self.is_legal_move, possible_moves))
                 possible_moves = list(filter(lambda pos: pos not in tower_pos_set, possible_moves))
                 possible_moves_dict[tower] = possible_moves
+        else:
+            for tower in self.tower_can_play:
+                possible_moves = []
+                tower_y, tower_x = self.white_player_pos[tower]
+                possible_moves += [(i, tower_x) for i in range(8) if i > tower_y]
+                sum_ = tower_y + tower_x
+                # right diagonal
+                possible_moves += [(i, sum_ - i) for i in range(8) if i > tower_y]
+                # left diagonal
+                possible_moves += [(i, 2 * tower_x - (sum_ - i)) for i in range(8) if i > tower_y]
+                possible_moves = list(filter(self.is_legal_move, possible_moves))
+                possible_moves = list(filter(lambda pos: pos not in tower_pos_set, possible_moves))
+                possible_moves_dict[tower] = possible_moves
 
         return possible_moves_dict
 
-    def move_tower(self, color, pos):
-        pass
-        # new_black_player_pos =
-        # new_white_player_pos
+    def move_tower(self, tower, pos):
+        assert isinstance(pos, tuple)
+        new_black_player_pos = dict(self.black_player_pos)
+        new_white_player_pos = dict(self.white_player_pos)
+        if self.current_player == Player.WHITE:
+            new_white_player_pos[tower] = pos
+            current_player = Player.BLACK
+        else:
+            new_black_player_pos[tower] = pos
+            current_player = Player.WHITE
+        # tower_can_play = [boa]
+        return Kamisado(new_black_player_pos, new_white_player_pos, current_player)
--- KamisadoGame/test_kamisado.py
+++ KamisadoGame/test_kamisado.py
@@ -4,14 +4,27 @@ from KamisadoGame.kamisado import Kamisado
 
 
 class TestKamisado(TestCase):
-    def test_start_game_white_has_102_moves(self):
-        game = Kamisado()
-        white_moves = game.get_possible_moves()
+    def test_start_game_white_moves(self):
+        board = Kamisado()
+        white_moves = board.get_possible_moves()
         self.assertEqual(12, len(white_moves['Brown']))
         self.assertEqual(13, len(white_moves['Green']))
 
-    def test_second_move_have_15_moves(self):
-        game = Kamisado()
-        white_moves = game.get_possible_moves()
+    def test_move_tower(self):
+        board = Kamisado()
+        white_moves = board.get_possible_moves()
+        tower = 'Red'
+        tower_old_pos = board.white_player_pos[tower]
+        tower_new_pos = white_moves[tower][3]
+        new_board = board.move_tower(tower, tower_new_pos)
+        self.assertEqual(board.white_player_pos[tower], tower_old_pos)
+        self.assertEqual(new_board.white_player_pos[tower], tower_new_pos)
 
-
+    def test_second_move_black(self):
+        board = Kamisado()
+        white_moves = board.get_possible_moves()
+        new_board = board.move_tower('Brown', (6, 0))
+        black_moves = new_board.get_possible_moves()
+        self.assertIn('Purple', black_moves)
+        self.assertEqual(1, len(black_moves))
+        self.assertEqual(13, len(black_moves['Purple']))
